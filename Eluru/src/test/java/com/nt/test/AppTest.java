a266539ddcdc96ac7ba5f1690cd18ddc2ecd0358
Narayana <Narayana@192.168.1.14>
2024-10-25 10:43:53 +0530

    AppTest.java 14 line is modified

--- Eluru/src/test/java/com/nt/test/AppTest.java
+++ Eluru/src/test/java/com/nt/test/AppTest.java
@@ -11,8 +11,8 @@ public class AppTest {
 	@Test
 	public void testWithPositives() {
 		Arithmatic a=new Arithmatic();
-		 int expected=40;
-		 int  actual=a.sum(20, 20); 
+		 int expected=60;
+		 int  actual=a.sum(40, 20); 
 		 assertEquals(expected, actual);
 	}
 	@Test
